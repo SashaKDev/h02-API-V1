eddd849f4a45a646d3da85e605db3ce5023ce689
Alexander <sasha.kozel252000@gmail.com>
2025-11-19 16:57:56 +0300

    commit

--- src/core/middlewares/validation/inputValidationResult.ts
+++ src/core/middlewares/validation/inputValidationResult.ts
@@ -1,14 +1,22 @@
 import {NextFunction, Request, Response} from "express";
-import {FieldValidationError, validationResult} from "express-validator";
+import {FieldValidationError, ValidationError, validationResult} from "express-validator";
 
 
 export const inputValidationResult = (req: Request, res: Response, next: NextFunction) => {
 
-    const errors = validationResult(req).array() as FieldValidationError[];
+    const errorFormatter = (validationError: ValidationError) => {
+        const fieldValidationError = validationError as FieldValidationError;
 
+        return {
+            field: fieldValidationError.path,
+            message: fieldValidationError.msg,
+        }
+    }
+
+    const errors = validationResult(req).formatWith(errorFormatter).array({onlyFirstError: true});
 
     if (errors.length) {
-        res.status(400).json({message: errors[0].msg, field: errors[0].path});
+        res.status(400).json(errors);
     }
     next();
 }
