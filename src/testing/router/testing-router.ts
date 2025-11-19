c99b4cea8463dbf0196271b4a622c2e8475a17e0
Alexander <sasha.kozel252000@gmail.com>
2025-11-19 14:54:21 +0300

    commit

---
+++ src/testing/router/testing-router.ts
@@ -0,0 +1,11 @@
+import {Router, Request, Response} from "express";
+import {basicAuthMiddleware} from "../../auth/middlewares/basicAuthMiddleware";
+import {db} from "../../db/in-memory.db";
+
+
+export const testingRouter = Router({});
+
+testingRouter.delete('/all-data', basicAuthMiddleware, (req: Request, res: Response) => {
+    db.blogs = [];
+    res.sendStatus(204);
+});
