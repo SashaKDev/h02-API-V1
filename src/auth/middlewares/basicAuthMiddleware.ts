c82e0784dbd245c3b626e5ee0f7091d8fd464439
Alexander <sasha.kozel252000@gmail.com>
2025-11-19 15:09:25 +0300

    commit

--- src/auth/middlewares/basicAuthMiddleware.ts
+++ src/auth/middlewares/basicAuthMiddleware.ts
@@ -1,24 +1,28 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from 'express';
 
-const ADMIN_USERNAME = "admin";
-const ADMIN_PASSWORD = "qwerty";
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin';
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'qwerty';
 
-export const basicAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const auth = req.headers["authorization"];
+export const superAdminGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const auth = req.headers['authorization'] as string; // 'Basic xxxx'
     if (!auth) {
         res.sendStatus(401);
         return;
     }
-    const [authType, token] = auth.split(" ");
+
+    const [authType, token] = auth.split(' '); //admin:qwerty
     if (authType !== 'Basic') {
-        res.sendStatus(401);
-        return
-    }
-    const decodedAuth = Buffer.from(token, 'base64').toString('utf-8');
-    const [username, password] = decodedAuth.split(":");
-    if(username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
         res.sendStatus(401);
         return;
     }
-    next();
-}
+
+    const credentials = Buffer.from(token, 'base64').toString('utf-8');
+    const [username, password] = credentials.split(':');
+
+    if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+        res.sendStatus(401);
+        return;
+    }
+
+    next(); // Успешная авторизация, продолжаем
+};
