10dcecb11791eed7ce5023da64b976010e8afa7d
Alexander <sasha.kozel252000@gmail.com>
2025-11-19 17:59:26 +0300

    commit

---
+++ src/posts/router/postsRouter.ts
@@ -0,0 +1,70 @@
+import {Router, Request, Response} from "express";
+import {db} from "../../db/in-memory.db";
+import {basicAuthMiddleware} from "../../auth/middlewares/basicAuthMiddleware";
+import {postInputDtoValidation} from "../validation/postInputDtoValidation";
+import {inputValidationResult} from "../../core/middlewares/validation/inputValidationResult";
+import {Post} from "../types/post";
+import {idValidation} from "../../core/middlewares/validation/paramValidation";
+
+export const postsRouter = Router({});
+
+postsRouter.get('/', (req: Request, res: Response) => {
+    res.status(200).json(db.posts);
+});
+
+postsRouter.get('/:id', (req: Request, res: Response) => {
+    const foundPost = db.posts.find(post => post.id === req.params.id);
+    if (!foundPost) {
+        res.sendStatus(404);
+    }
+    res.status(200).json(foundPost);
+});
+
+postsRouter.post('/', basicAuthMiddleware, postInputDtoValidation, inputValidationResult, (req: Request, res: Response) => {
+    const foundBlog = db.blogs.find(blog => blog.id === req.body.blogId);
+    if (!foundBlog) {
+        res.sendStatus(404);
+        return;
+    }
+    const newPost: Post = {
+        id: (db.posts.length + 1).toString(),
+        title: req.body.title,
+        shortDescription: req.body.shortDescription,
+        content: req.body.content,
+        blogId: req.body.blogId,
+        blogName: foundBlog.name
+    }
+    res.status(201).json({newPost});
+});
+
+postsRouter.put('/:id', basicAuthMiddleware, idValidation, postInputDtoValidation, inputValidationResult, (req: Request, res: Response) => {
+    const foundPost = db.posts.find(post => post.id === req.params.id);
+    if (!foundPost) {
+        res.sendStatus(404);
+        return;
+    }
+    const foundBlog = db.blogs.find(blog => blog.id === req.body.blogId);
+    if (!foundBlog) {
+        res.sendStatus(404);
+        return;
+    }
+    foundPost.title = req.body.title;
+    foundPost.shortDescription = req.body.shortDescription;
+    foundPost.content = req.body.content;
+    foundPost.blogId = req.body.blogId;
+    foundPost.blogName = foundBlog.name
+
+
+    res.sendStatus(204);
+});
+
+postsRouter.delete('/:id', basicAuthMiddleware, idValidation,inputValidationResult, (req: Request, res: Response) => {
+    const foundPost = db.posts.find(post => post.id === req.params.id);
+    if (!foundPost) {
+        res.sendStatus(404);
+        return;
+    }
+
+    db.posts = db.posts.filter(post => post.id !== req.params.id)
+    res.sendStatus(204);
+})
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,6 +2,7 @@ import express, {Express, Request, Response} from "express";
 import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/paths/paths";
 import {blogsRouter} from "./blogs/router/blogs-router";
 import {testingRouter} from "./testing/router/testing-router";
+import {postsRouter} from "./posts/router/postsRouter";
 
 
 export const setupApp = (app: Express) => {
@@ -12,7 +13,7 @@ export const setupApp = (app: Express) => {
         res.json({message: 'Hello World!'});
     })
     app.use(BLOGS_PATH, blogsRouter);
-    // app.use(POSTS_PATH, postsRouter);
+    app.use(POSTS_PATH, postsRouter);
     app.use(TESTING_PATH, testingRouter);
 
     return app;
