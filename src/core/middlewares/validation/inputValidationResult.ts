c985ee051254f2fde2c20cb58049014ee0d51da3
Alexander <sasha.kozel252000@gmail.com>
2025-11-19 16:25:32 +0300

    commit

--- src/core/middlewares/validation/inputValidationResult.ts
+++ src/core/middlewares/validation/inputValidationResult.ts
@@ -4,7 +4,12 @@ import {validationResult} from "express-validator";
 
 export const inputValidationResult = (req: Request, res: Response, next: NextFunction) => {
 
-    const errors = validationResult(req).array();
+    const formatErrors = () => {
+
+    }
+
+    const errors = validationResult(req).formatWith((error) => {error.msg}).array()
+
     if (errors.length) {
         res.status(400).json(errors);
     }
